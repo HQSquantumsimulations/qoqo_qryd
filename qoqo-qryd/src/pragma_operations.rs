b48fcedd9ad4bcc6862f8d98979ebf49c55fb4b6
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2023-09-04 15:04:46 +0200

    PragmaDeactivateQRydQubit impl + int

--- qoqo-qryd/src/pragma_operations.rs
+++ qoqo-qryd/src/pragma_operations.rs
@@ -22,7 +22,7 @@ use pyo3::types::PyByteArray;
 use pyo3::types::PySet;
 use qoqo::operations::PragmaChangeDeviceWrapper;
 use roqoqo::prelude::*;
-use roqoqo_qryd::{PragmaChangeQRydLayout, PragmaShiftQRydQubit};
+use roqoqo_qryd::{PragmaChangeQRydLayout, PragmaDeactivateQRydQubit, PragmaShiftQRydQubit};
 use std::collections::HashMap;
 
 #[pyclass(
@@ -534,6 +534,265 @@ impl PragmaShiftQRydQubitWrapper {
     }
 }
 
+#[pyclass(
+    name = "PragmaDeactivateQRydQubit",
+    module = "qoqo_qryd.pragma_operations"
+)]
+#[derive(Clone, Debug, PartialEq, Eq)]
+/// This PRAGMA Operation deactivates a qubit in a QRyd Experimental device.
+///
+/// In QRyd Experimental devices a quantum state is trapped within an optical tweezer.
+/// This Operation signals the device to drop the quantum state related to the given qubit.
+///
+/// Args:
+///     qubit (int): The qubit to deactivate.
+pub struct PragmaDeactivateQRydQubitWrapper {
+    /// PragmaDeactivateQRydQubit to be wrapped and converted to Python.
+    pub internal: PragmaDeactivateQRydQubit,
+}
+
+#[pymethods]
+impl PragmaDeactivateQRydQubitWrapper {
+    /// Create a PragmaDeactivateQRydQubit.
+    ///
+    /// Args:
+    ///     qubit (int): The qubit to deactivate.
+    ///
+    /// Returns:
+    ///     self: The new PragmaDeactivateQRydQubit.
+    #[new]
+    #[pyo3(text_signature = "(qubit, /)")]
+    fn new(qubit: usize) -> Self {
+        Self {
+            internal: PragmaDeactivateQRydQubit::new(qubit),
+        }
+    }
+
+    /// Return the qubit involved in the Operation.
+    ///
+    ///
+    /// Returns:
+    ///     int: The qubit involved in the Operation.
+    fn qubit(&self) -> usize {
+        self.internal.qubit
+    }
+
+    /// Wrap PragmaDeactivateQRydQubit in PragmaChangeDevice operation
+    ///
+    /// PragmaDeactivateQRydQubit is device specific and can not be directly added to a Circuit.
+    /// Instead it is first wrapped in a PragmaChangeDevice operation that is in turn added
+    /// to the circuit.
+    ///
+    /// Example
+    /// -------
+    ///
+    /// >>> from qoqo import Circuit
+    /// ... from qoqo_qryd.pragma_operations import PragmaDeactivateQRydQubit
+    /// ... circuit = Circuit()
+    /// ... circuit += PragmaDeactivateQRydQubit(qubit=0).to_pragma_change_device()
+    ///
+    /// Returns:
+    ///     PragmaChangeDevice
+    pub fn to_pragma_change_device(&self) -> PyResult<PragmaChangeDeviceWrapper> {
+        Ok(PragmaChangeDeviceWrapper {
+            internal: self.internal.to_pragma_change_device().map_err(|err| {
+                PyRuntimeError::new_err(format!(
+                    "Error occured during serialisation of PragmaDeactivateQRydQubit {:?}",
+                    err
+                ))
+            })?,
+        })
+    }
+
+    /// List all involved qubits.
+    ///
+    /// Returns:
+    ///     set[int]: The involved qubits of the PRAGMA operation.
+    fn involved_qubits(&self) -> PyObject {
+        let pyobject: PyObject = Python::with_gil(|py| -> PyObject {
+            PySet::new(py, &[self.internal.qubit])
+                .unwrap()
+                .to_object(py)
+        });
+        pyobject
+    }
+
+    /// Return tags classifying the type of the operation.
+    ///
+    /// Used for the type based dispatch in ffi interfaces.
+    ///
+    /// Returns:
+    ///     list[str]: The tags of the operation.
+    fn tags(&self) -> Vec<String> {
+        self.internal.tags().iter().map(|s| s.to_string()).collect()
+    }
+
+    /// Return hqslang name of the operation.
+    ///
+    /// Returns:
+    ///     str: The hqslang name of the operation.
+    fn hqslang(&self) -> &'static str {
+        self.internal.hqslang()
+    }
+
+    /// Return true when the operation has symbolic parameters.
+    ///
+    /// Returns:
+    ///     bool: True if the operation contains symbolic parameters, False if it does not.
+    fn is_parametrized(&self) -> bool {
+        self.internal.is_parametrized()
+    }
+
+    /// Return the bincode representation of the PragmaDeactivateQRydQubit using the bincode crate.
+    ///
+    /// Returns:
+    ///     ByteArray: The serialized PragmaDeactivateQRydQubit (in bincode form).
+    ///
+    /// Raises:
+    ///     ValueError: Cannot serialize PragmaDeactivateQRydQubit to bytes.
+    pub fn to_bincode(&self) -> PyResult<Py<PyByteArray>> {
+        let serialized = serialize(&self.internal).map_err(|_| {
+            PyValueError::new_err("Cannot serialize PragmaDeactivateQRydQubit to bytes")
+        })?;
+        let b: Py<PyByteArray> = Python::with_gil(|py| -> Py<PyByteArray> {
+            PyByteArray::new(py, &serialized[..]).into()
+        });
+        Ok(b)
+    }
+
+    /// Convert the bincode representation of the PragmaDeactivateQRydQubit to a PragmaDeactivateQRydQubit using the bincode crate.
+    ///
+    /// Args:
+    ///     input (ByteArray): The serialized PragmaDeactivateQRydQubit (in bincode form).
+    ///
+    /// Returns:
+    ///     PragmaDeactivateQRydQubit: The deserialized PragmaDeactivateQRydQubit.
+    ///
+    /// Raises:
+    ///     TypeError: Input cannot be converted to byte array.
+    ///     ValueError: Input cannot be deserialized to PragmaDeactivateQRydQubit.
+    #[pyo3(text_signature = "(input, /)")]
+    pub fn from_bincode(&self, input: &PyAny) -> PyResult<PragmaDeactivateQRydQubitWrapper> {
+        let bytes = input
+            .extract::<Vec<u8>>()
+            .map_err(|_| PyTypeError::new_err("Input cannot be converted to byte array"))?;
+
+        Ok(PragmaDeactivateQRydQubitWrapper {
+            internal: deserialize(&bytes[..])
+                .map_err(|_| PyValueError::new_err("Input cannot be deserialized to Circuit"))?,
+        })
+    }
+
+    /// Substitute the symbolic parameters in a clone of the PRAGMA operation according to the substitution_parameters input.
+    ///
+    /// Args:
+    ///     substitution_parameters (dict[str, float]): The dictionary containing the substitutions to use in the PRAGMA operation.
+    ///
+    /// Returns:
+    ///     self: The PRAGMA operation operation with the parameters substituted.
+    ///
+    /// Raises:
+    ///     RuntimeError: The parameter substitution failed.
+    #[pyo3(text_signature = "(substitution_parameters, /)")]
+    fn substitute_parameters(
+        &self,
+        substitution_parameters: std::collections::HashMap<&str, f64>,
+    ) -> PyResult<Self> {
+        let mut calculator = qoqo_calculator::Calculator::new();
+        for (key, val) in substitution_parameters.iter() {
+            calculator.set_variable(key, *val);
+        }
+        Ok(Self {
+            internal: self
+                .internal
+                .substitute_parameters(&calculator)
+                .map_err(|x| {
+                    pyo3::exceptions::PyRuntimeError::new_err(format!(
+                        "Parameter Substitution failed: {:?}",
+                        x
+                    ))
+                })?,
+        })
+    }
+
+    /// Remap qubits in a clone of the PRAGMA operation.
+    ///
+    /// Args:
+    ///     mapping (dict[int, int]): The dictionary containing the {qubit: qubit} mapping to use in the PRAGMA operation.
+    ///
+    /// Returns:
+    ///     self: The PRAGMA operation with the qubits remapped.
+    ///
+    /// Raises:
+    ///     RuntimeError: The qubit remapping failed.
+    #[pyo3(text_signature = "(mapping, /)")]
+    fn remap_qubits(&self, mapping: std::collections::HashMap<usize, usize>) -> PyResult<Self> {
+        let new_internal = self
+            .internal
+            .remap_qubits(&mapping)
+            .map_err(|_| pyo3::exceptions::PyRuntimeError::new_err("Qubit remapping failed: "))?;
+        Ok(Self {
+            internal: new_internal,
+        })
+    }
+
+    /// Return a copy of the PRAGMA operation (copy here produces a deepcopy).
+    ///
+    /// Returns:
+    ///     PragmaDeactivateQRydQubit: A deep copy of self.
+    fn __copy__(&self) -> PragmaDeactivateQRydQubitWrapper {
+        self.clone()
+    }
+
+    /// Return a deep copy of the PRAGMA operation.
+    ///
+    /// Returns:
+    ///     PragmaDeactivateQRydQubit: A deep copy of self.
+    fn __deepcopy__(&self, _memodict: Py<PyAny>) -> PragmaDeactivateQRydQubitWrapper {
+        self.clone()
+    }
+
+    /// Return a string containing a formatted (string) representation of the PRAGMA operation.
+    ///
+    /// Returns:
+    ///     str: The string representation of the operation.
+    fn __format__(&self, _format_spec: &str) -> PyResult<String> {
+        Ok(format!("{:?}", self.internal))
+    }
+
+    /// Return a string containing a printable representation of the PRAGMA operation.
+    ///
+    /// Returns:
+    ///     str: The printable string representation of the operation.
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!("{:?}", self.internal))
+    }
+
+    /// Return the __richcmp__ magic method to perform rich comparison operations on PragmaDeactivateQRydQubit.
+    ///
+    /// Args:
+    ///     self: The PragmaDeactivateQRydQubit object.
+    ///     other: The object to compare self to.
+    ///     op: Whether they should be equal or not.
+    ///
+    /// Returns:
+    ///     bool: Whether the two operations compared evaluated to True or False.
+    fn __richcmp__(&self, other: Py<PyAny>, op: pyo3::class::basic::CompareOp) -> PyResult<bool> {
+        let other: PragmaDeactivateQRydQubitWrapper =
+            Python::with_gil(|py| -> PyResult<PragmaDeactivateQRydQubitWrapper> {
+                let other_extracted: PyResult<PragmaDeactivateQRydQubitWrapper> = other.extract(py);
+                other_extracted
+            })?;
+        match op {
+            pyo3::class::basic::CompareOp::Eq => Ok(self.internal == other.internal),
+            pyo3::class::basic::CompareOp::Ne => Ok(self.internal != other.internal),
+            _ => Err(pyo3::exceptions::PyNotImplementedError::new_err(
+                "Other comparison not implemented.",
+            )),
+        }
+    }
+}
+
 /// QRyd specific PragmaOperations that support changing the QRyd device during a circuit evaluation
 ///
 /// .. autosummary::
@@ -541,9 +800,11 @@ impl PragmaShiftQRydQubitWrapper {
 ///
 ///    PragmaChangeQRydLayout
 ///    PragmaShiftQRydQubit
+///    PragmaDeactivateQRydQubit
 #[pymodule]
 pub fn pragma_operations(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PragmaChangeQRydLayoutWrapper>()?;
     m.add_class::<PragmaShiftQRydQubitWrapper>()?;
+    m.add_class::<PragmaDeactivateQRydQubitWrapper>()?;
     Ok(())
 }
