635efbb0b1217ac97d34887de3e1a28a05c3597e
mlodi-hqs <matteo.lodi@quantumsimulations.de>
2024-09-30 17:34:31 +0200

    solution

--- roqoqo-qryd/src/lib.rs
+++ roqoqo-qryd/src/lib.rs
@@ -74,6 +74,11 @@ pub mod api_backend;
 #[cfg(feature = "web-api")]
 pub use api_backend::*;
 
+#[cfg(feature = "web-api")]
+use roqoqo::RoqoqoBackendError;
+#[cfg(feature = "web-api")]
+use std::env;
+
 /// Compute the angle according to the appropriate relation and phi/theta values.
 ///
 /// # Arguments
@@ -100,5 +105,151 @@ pub fn phi_theta_relation(relation_name: &str, mut theta: f64) -> Option<f64> {
                     * f64::ln(1.63085 * theta * theta * f64::exp(2.0 * theta) + theta + 0.02889),
         ),
         _ => None,
+    }
+}
+
+/// Enum for a Device that can be a TweezerDevice or an EmulatorDevice.
+#[derive(Debug)]
+pub enum CombinedDevice {
+    /// Variant for Tweezer devices
+    Tweezer(TweezerDevice),
+    /// Variant for Emulator devices
+    Emulator(EmulatorDevice),
+}
+
+/// Creates a new TweezerDevice instance containing populated tweezer data or EmulatorDevice instance.
+///
+/// This requires a valid QRYD_API_TOKEN. Visit `https://thequantumlaend.de/get-access/` to get one.
+///
+/// # Arguments
+///
+/// * `device_name` - The name of the device to instantiate. Defaults to "qryd_emulator".
+/// * `access_token` - An access_token is required to access QRYD hardware and emulators.
+///                    The access_token can either be given as an argument here
+///                         or set via the environmental variable `$QRYD_API_TOKEN`.
+/// * `seed` - Optionally overwrite seed value from downloaded device instance.
+/// * `dev` - The boolean to set the dev header to.
+/// * `api_version` - The version of the QRYD API to use. Defaults to "v1_1".
+///
+/// # Returns
+///
+/// * `CombinedDevice` - The new CombinedDevice instance, with variant TweezerDevice or
+///     EmulatorDevice depending on the pulled information.
+///
+/// # Errors
+///
+/// * `RoqoqoBackendError`
+#[cfg(feature = "web-api")]
+pub fn device_from_api(
+    device_name: Option<String>,
+    access_token: Option<String>,
+    seed: Option<usize>,
+    dev: Option<bool>,
+    api_version: Option<String>,
+) -> Result<CombinedDevice, RoqoqoBackendError> {
+    // Preparing variables
+    let device_name_internal = device_name.unwrap_or_else(|| String::from("qryd_emulator"));
+    let api_version = api_version.unwrap_or_else(|| String::from("v1_1"));
+    let dev = dev.unwrap_or(false);
+    let hqs_env_var = env::var("QRYD_API_HQS").is_ok();
+    let access_token_internal: String = match access_token {
+        Some(s) => s,
+        None => {
+            env::var("QRYD_API_TOKEN").map_err(|_| RoqoqoBackendError::MissingAuthentication {
+                msg: "QRYD access token is missing.".to_string(),
+            })?
+        }
+    };
+
+    // Client setup
+    let client = reqwest::blocking::Client::builder()
+        .https_only(true)
+        .build()
+        .map_err(|x| RoqoqoBackendError::NetworkError {
+            msg: format!("Could not create https client {:?}.", x),
+        })?;
+
+    // Response gathering
+    let resp = match (dev, hqs_env_var) {
+        (true, true) => client
+            .get(format!(
+                "https://api.qryddemo.itp3.uni-stuttgart.de/{}/devices/{}",
+                api_version, device_name_internal
+            ))
+            .header("X-API-KEY", access_token_internal)
+            .header("X-DEV", "?1")
+            .header("X-HQS", "?1")
+            .send()
+            .map_err(|e| RoqoqoBackendError::NetworkError {
+                msg: format!("{:?}", e),
+            })?,
+        (true, false) => client
+            .get(format!(
+                "https://api.qryddemo.itp3.uni-stuttgart.de/{}/devices/{}",
+                api_version, device_name_internal
+            ))
+            .header("X-API-KEY", access_token_internal)
+            .header("X-DEV", "?1")
+            .send()
+            .map_err(|e| RoqoqoBackendError::NetworkError {
+                msg: format!("{:?}", e),
+            })?,
+        (false, true) => client
+            .get(format!(
+                "https://api.qryddemo.itp3.uni-stuttgart.de/{}/devices/{}",
+                api_version, device_name_internal
+            ))
+            .header("X-API-KEY", access_token_internal)
+            .header("X-HQS", "?1")
+            .send()
+            .map_err(|e| RoqoqoBackendError::NetworkError {
+                msg: format!("{:?}", e),
+            })?,
+        (false, false) => client
+            .get(format!(
+                "https://api.qryddemo.itp3.uni-stuttgart.de/{}/devices/{}",
+                api_version, device_name_internal
+            ))
+            .header("X-API-KEY", access_token_internal)
+            .send()
+            .map_err(|e| RoqoqoBackendError::NetworkError {
+                msg: format!("{:?}", e),
+            })?,
+    };
+
+    // Response handling
+    let status_code = resp.status();
+    if status_code == reqwest::StatusCode::OK {
+        if let Ok(mut device) = resp.json::<TweezerDevice>() {
+            if device.available_gates.is_some() {
+                if let Some(new_seed) = seed {
+                    device.seed = Some(new_seed);
+                }
+                device.device_name = device_name_internal;
+                Ok(CombinedDevice::Emulator(EmulatorDevice {
+                    internal: device,
+                }))
+            } else {
+                if let Some(default) = device.default_layout.clone() {
+                    device.switch_layout(&default, None).unwrap();
+                }
+                if let Some(new_seed) = seed {
+                    device.seed = Some(new_seed);
+                }
+                device.device_name = device_name_internal;
+                Ok(CombinedDevice::Tweezer(device))
+            }
+        } else {
+            Err(RoqoqoBackendError::GenericError {
+                msg: "Failed deserialization from device_from_api().".to_string(),
+            })
+        }
+    } else {
+        Err(RoqoqoBackendError::NetworkError {
+            msg: format!(
+                "Request to server failed with HTTP status code {:?}.",
+                status_code
+            ),
+        })
     }
 }
